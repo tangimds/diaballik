e2790928d7d38f6ecd6cc39ef3c21220209a16b7
Macbook Tangi <tangimds@gmail.com>
2018-11-30 19:52:36 +0100

    Légères corrections de PVCGameBuilder et PVPGameBuilder
     - PVP : correction de la couleur de P2
     - PVC : ajout du setter de l'attribut level de Game

--- diaballik-model/src/main/java/diaballik/model/PvPGameBuilder.java
+++ diaballik-model/src/main/java/diaballik/model/PvPGameBuilder.java
@@ -5,7 +5,7 @@ public class PvPGameBuilder extends GameBuilder {
 	@Override
 	public Game buildGame(final String name1, final String name2, final int scenario, final int difficulty) {
 		final Player whitePlayer = new HumanPlayer(name1, Color.WHITE);
-		final Player blackPlayer = new HumanPlayer(name2, Color.WHITE);
+		final Player blackPlayer = new HumanPlayer(name2, Color.BLACK);
 		final Scenario scnr = Scenario.values()[scenario];
 
 		return new Game(whitePlayer, blackPlayer, scnr);
