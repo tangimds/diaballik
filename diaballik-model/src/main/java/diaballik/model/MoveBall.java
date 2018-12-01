189853bd60210f45faf05c853ab017f1cb4dc7ab
Macbook Tangi <tangimds@gmail.com>
2018-12-01 21:46:28 +0100

    Modification de la classe MoveBall
    verification supplémentaires :
     - passe effectuée entre coéquipiers
     - passeur possède la balle
    
    modification des tests sales dans le main

--- diaballik-model/src/main/java/diaballik/model/MoveBall.java
+++ diaballik-model/src/main/java/diaballik/model/MoveBall.java
@@ -25,18 +25,34 @@ public class MoveBall implements Action {
 		final boolean moveDiagonal = Math.abs(startingPiece.getX() - endingPiece.getX()) == Math.abs(startingPiece.getY() - endingPiece.getY());
 		final boolean moveAuthorized = moveDiagonal || moveHorizontal || moveVertical;
 
+		//check the teams
+		final boolean sameTeam = startingPiece.getColor() == endingPiece.getColor();
+		final boolean hasTheBall;
+
+		//check if the launcher has the ball
+		if (startingPiece.getColor() == Color.WHITE) {
+			hasTheBall = startingPiece.equals(b.getCurrentWhiteHolder());
+		} else {
+			hasTheBall = startingPiece.equals(b.getCurrentBlackHolder());
+		}
+
 		final boolean[] enemyOnTheWay = new boolean[1];
-		enemyOnTheWay[0]=false;
+		enemyOnTheWay[0] = false;
 
 		b.getPieces().stream()
-				.filter(p->p.isBetween(startingPiece,endingPiece))
+				.filter(p -> p.isBetween(startingPiece, endingPiece))
 				.forEach(pLine -> {
-					if(pLine.getColor()!=startingPiece.getColor())
-						enemyOnTheWay[0] =true;
+					if (pLine.getColor() != startingPiece.getColor()) {
+						enemyOnTheWay[0] = true;
+					}
 				});
 
+		/*System.out.println("moveAuthorized : "+moveAuthorized);
+		System.out.println("enemy on the way : "+enemyOnTheWay[0]);
+		System.out.println("same team : "+sameTeam);
+		System.out.println("has the ball : "+hasTheBall);*/
 
-		return moveAuthorized && !enemyOnTheWay[0];
+		return moveAuthorized && !enemyOnTheWay[0] && sameTeam && hasTheBall;
 	}
 
 	@Override
@@ -55,20 +71,34 @@ public class MoveBall implements Action {
 	}
 
 	public static void main(final String[] args) {
-		final Piece p1 = new Piece(Color.WHITE,5,4);
-		final Piece p2 = new Piece(Color.WHITE,3,2);
-		final Piece p3 = new Piece(Color.WHITE,7,6);
-		final Piece p4 = new Piece(Color.WHITE,7,3);
-		final Piece p5 = new Piece(Color.WHITE,2,4);
-		final Piece p6 = new Piece(Color.WHITE,3,4);
-		final Piece p7 = new Piece(Color.WHITE,3,6);
-		final Piece p8 = new Piece(Color.WHITE,4,5);
+		final Piece p1 = new Piece(Color.WHITE, 5, 4);
+		final Piece p2 = new Piece(Color.WHITE, 3, 2);
+		final Piece p3 = new Piece(Color.WHITE, 7, 6);
+		final Piece p4 = new Piece(Color.WHITE, 7, 3);
+		final Piece p5 = new Piece(Color.WHITE, 2, 4);
+		final Piece p6 = new Piece(Color.WHITE, 3, 4);
+		final Piece p7 = new Piece(Color.WHITE, 3, 6);
+		final Piece p8 = new Piece(Color.WHITE, 4, 5);
 
-		final MoveBall mb = new MoveBall(p1,p2);
-		//System.out.println("true"+mb.onTheSameLine(p1,p2,p3)+mb.onTheSameLine(p1,p8,p7)+mb.onTheSameLine(p1,p6,p5));
+		final MoveBall mb = new MoveBall(p1, p2);
+		System.out.println("**** true ****");
+		System.out.println(">" + p1.isBetween(p2, p3));
+		System.out.println(">" + p1.isBetween(p3, p2));
+		System.out.println(">" + p8.isBetween(p7, p1));
+		System.out.println(">" + p6.isBetween(p5, p1));
+		System.out.println(">" + p6.isBetween(p2, p7));
+
+		System.out.println("**** false **** ");
+		System.out.println(">" + p1.isBetween(p8, p7));
+		System.out.println(">" + p5.isBetween(p6, p1));
+		System.out.println(">" + p1.isBetween(p4, p7));
+
+
 		//System.out.println("false"+mb.onTheSameLine(p1,p2,p4)+mb.onTheSameLine(p2,p4,p5));
-		final int x1=-1, y1=-2, x2=3 , y2=6;
-		System.out.println((x1*x2+y1*y2) / (Math.sqrt((x1*x1)+(y1*y1))*Math.sqrt((x2*x2)+(y2*y2))));
+
+
+		//final int x1=-1, y1=-2, x2=3 , y2=6;
+		//System.out.println((x1*x2+y1*y2) / (Math.sqrt((x1*x1)+(y1*y1))*Math.sqrt((x2*x2)+(y2*y2))));
 
 	}
 
