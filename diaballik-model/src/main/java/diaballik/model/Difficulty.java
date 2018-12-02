c528244dbb1655695c047009c3668c2a2eca139f
taha-yassine <yassinetaha1997@gmail.com>
2018-12-02 03:20:44 +0100

    Mise à jour des enum Difficulty et Scenario

--- diaballik-model/src/main/java/diaballik/model/Difficulty.java
+++ diaballik-model/src/main/java/diaballik/model/Difficulty.java
@@ -1,3 +1,15 @@
 package diaballik.model;
 
-public enum Difficulty {NOOB, STARTING, PROGRESSIVE};
+public enum Difficulty {
+	NOOB(0), STARTING(1), PROGRESSIVE(1);
+
+	private int value;
+
+	private Difficulty(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+};
--- diaballik-model/src/main/java/diaballik/model/Scenario.java
+++ diaballik-model/src/main/java/diaballik/model/Scenario.java
@@ -1,3 +1,15 @@
 package diaballik.model;
 
-public enum Scenario {STANDARD, RANDOM, EAU};
+public enum Scenario {
+	STANDARD(0), RANDOM(1), EAU(2);
+
+	private int value;
+
+	private Scenario(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+};
