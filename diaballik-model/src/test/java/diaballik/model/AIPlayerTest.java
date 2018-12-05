b8ae39b96f71f8277d2a59a6bb8502a9a90d6475
Mendes Tangi <tangimds@gmail.com>
2018-12-05 01:06:44 +0100

    Modification tests

--- diaballik-model/src/test/java/diaballik/model/AIPlayerTest.java
+++ diaballik-model/src/test/java/diaballik/model/AIPlayerTest.java
@@ -6,14 +6,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AIPlayerTest {
-	AIPlayer p;
-	@BeforeEach
-	void setUp() {
-		this.p = new AIPlayer("ordi", Color.BLACK, Difficulty.NOOB);
-	}
 
 	@Test
 	void testPlay() {
+		final StandardBoardBuilder standardBoardBuilder = new StandardBoardBuilder();
+		Board board = standardBoardBuilder.buildBoard();
+		AIPlayer ai = new AIPlayer("ordi", Color.BLACK, Difficulty.NOOB);
+		Board oldBoard = board.copy();
+		ai.play(board).execute(board);
+		assertFalse(oldBoard.equals(board));
 	}
 
 }
--- diaballik-model/src/test/java/diaballik/model/BoardTest.java
+++ diaballik-model/src/test/java/diaballik/model/BoardTest.java
@@ -2,6 +2,7 @@ package diaballik.model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Nested;
 
 import java.util.Optional;
 
@@ -17,14 +18,23 @@ class BoardTest {
 	@Test
 	void testAddPiece() {
 		Board board = new Board();
-		board.addPiece(new Piece(Color.WHITE,1,1));
-		assertFalse(board.getPieces().isEmpty(),"AddPiece");
+		board.addPiece(new Piece(Color.WHITE, 1, 1));
+		assertFalse(board.getPieces().isEmpty(), "AddPiece");
 	}
 
 	@Test
 	void testWin() {
+		Board board = new Board();
+		board.addPiece(new Piece(Color.WHITE, 1, 7));
+		Optional<Piece> optP = board.getPieces().stream()
+				.filter(p -> p.getX() == 1 && p.getY() == 7).findFirst();
+		optP.ifPresent(board::setCurrentWhiteHolder);
 
+		board.setCurrentBlackHolder(new Piece(Color.BLACK, 7, 7));
 
+		if (board.win().isPresent()) {
+			assertEquals(Color.WHITE, board.win().get(), "win");
+		}
 	}
 
 }
