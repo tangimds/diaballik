ae03e5efa25556176a247c97be73301358146b8e
taha-yassine <yassinetaha1997@gmail.com>
2018-12-05 12:23:15 +0100

    Implémentation de SavedGameBuilder

--- diaballik-model/src/main/java/diaballik/model/SavedGameBuilder.java
+++ diaballik-model/src/main/java/diaballik/model/SavedGameBuilder.java
@@ -1,9 +1,44 @@
 package diaballik.model;
 
-public class SavedGameBuilder {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import diaballik.serialization.DiabalikJacksonProvider;
 
-	public Game buildGame(final int gameId) {
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SavedGameBuilder extends GameBuilder {
+
+	@Override
+	public Game buildGame(String name1, String name2, Scenario scenario, Difficulty difficulty) {
 		return null;
+	}
+
+	@Override
+	public Game buildGame(final String gameId) {
+		final String dir = System.getProperty("user.dir") + System.getProperty("file.separator") + "Diaballik" + System.getProperty("file.separator");
+		final Path path = Paths.get(dir,"game_"+gameId+".txt");
+
+		byte[] encoded = new byte[0];
+		try {
+			encoded = Files.readAllBytes(path);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		final ObjectMapper mapper = new DiabalikJacksonProvider().getMapper();
+		Object readValue = null;
+		try {
+			readValue = mapper.readValue(new String(encoded, Charset.defaultCharset()), Game.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return (Game) readValue;
+
 	}
 
 }
