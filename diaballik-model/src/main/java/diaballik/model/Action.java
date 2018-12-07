d5ed3c469689af1e0c272b5c2d708ce0e1b052bb
Mendes Tangi <tangimds@gmail.com>
2018-12-07 10:28:17 +0100

    TODO Action.getColor

--- diaballik-model/src/main/java/diaballik/model/Action.java
+++ diaballik-model/src/main/java/diaballik/model/Action.java
@@ -18,4 +18,11 @@ public interface Action {
 
 	public abstract void undo(Board board);
 
+	public abstract void getColor(); //TODO
+	/*
+	moveBall et MovePiece auront leur propre getColor, qui retournera la couleur des pions liés à la commmande
+	cette couleur pourra alors être vérifiée lors du Game.play(Action a)
+	cela sert à verifier que le joueur bouge bien SES pions
+	 */
+
 }
