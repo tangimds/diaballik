45fb0159135b56eb8e54981bcf445e5ae1c9ca4f
taha-yassine <yassinetaha1997@gmail.com>
2018-12-10 14:23:10 +0100

    Bundle D2

--- diaballik-bundle/bundleD2.sh
+++ diaballik-bundle/bundleD2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-name1="prenom-NOM"
-name2="prenom-NOM"
+name1="Taha-YASSINE"
+name2="Tangi-MENDES"
 release="D2"-$name1-$name2
 
 # cleaning the release files
@@ -11,14 +11,14 @@ mkdir $release
 cd ../diaballik-model
 mvn clean package
 cd ..
-zip -r diaballik-bundle/$release/diaballik-D2-$name1-$name2-sources.zip diaballik-doc diaballik-model lib-algo -x diaballik-model/target/\* lib-algo/target/\* \*.idea/\* \*.iml
+zip -r diaballik-bundle/$release/diaballik-D2-$name1-$name2-sources.zip diaballik-doc diaballik-model -x diaballik-model/target/\* \*.idea/\* \*.iml
 
-# building Docker containers
+## building Docker containers
 ## Docker container of the back-end
-cd diaballik-model
-docker build -t diaballik-backend .
-cd ../diaballik-bundle
-docker save diaballik-backend | xz > $release/diaballik-$name1-$name2-backend-docker.tar.xz
+#cd diaballik-model
+#docker build -t diaballik-backend .
+#cd ../diaballik-bundle
+#docker save diaballik-backend | xz > $release/diaballik-$name1-$name2-backend-docker.tar.xz
 
 # docker stop diaballik-backend-run
 # docker rm diaballik-backend-run
