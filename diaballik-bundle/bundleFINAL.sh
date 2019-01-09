98e5519642f3ee556d65f6699bb3edce800dcf0d
Mendes Tangi <tangimds@gmail.com>
2019-01-09 22:53:38 +0100

    MaJ bundle

--- diaballik-bundle/bundleFINAL.sh
+++ diaballik-bundle/bundleFINAL.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-name1="prenom-NOM"
-name2="prenom-NOM"
+name1="tangi-MENDES"
+name2="taha-YASSINE"
 release="FINAL"-$name1-$name2
 
 # cleaning the release files
@@ -16,25 +16,5 @@ ng build --prod --build-optimizer
 cd ../diaballik-bundle
 mkdir $release
 cd ..
-zip -r diaballik-bundle/$release/diaballik-$name1-$name2-sources.zip diaballik-doc diaballik-model lib-algo diaballik-frontend/ -x diaballik-model/target/\* lib-algo/target/\* diaballik-frontend/dist/\* diaballik-frontend/node_modules/\* \*.idea/\* \*.iml
+zip -r diaballik-bundle/$release/diaballik-$name1-$name2-sources.zip diaballik-doc diaballik-model diaballik-frontend/ -x diaballik-model/target/\* lib-algo/target/\* diaballik-frontend/dist/\* diaballik-frontend/node_modules/\* \*.idea/\* \*.iml
 cd diaballik-bundle/
-
-# building Docker containers
-## Docker container of the back-end
-cd ../diaballik-model
-docker build -t diaballik-backend .
-# docker run -it --name diaballik-backend-run -d -p 4444:4444 diaballik-backend
-## Docker container of the front-end
-cd ../diaballik-frontend
-docker build -t diaballik-frontend .
-cd ../diaballik-bundle
-docker save diaballik-backend | xz > $release/diaballik-$name1-$name2-backend.tar.xz
-docker save diaballik-frontend | xz > $release/diaballik-$name1-$name2-frontend.tar.xz
-
-# docker stop diaballik-frontend-run
-# docker rm diaballik-frontend-run
-# docker run -it --name diaballik-frontend-run -d -p 8080:8080 diaballik-frontend
-
-# Docker stop/remove all the containers
-# docker stop $(docker ps -a -q)
-# docker rm $(docker ps -a -q)
